35696648ab1cdd73e65b0e0c6d48ad16b5cf95a9
NeonX <68928060+NeonX228@users.noreply.github.com>
2025-06-19 18:35:24 +0200

    Enemies system v1.0
    Added comments
    Fixed some bugs

--- Assets/Scripts/Enemies/EnemiesController.cs
+++ Assets/Scripts/Enemies/EnemiesController.cs
@@ -7,44 +7,137 @@ using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
+/// <summary>
+/// Defines the settings for enemy type that can spawn within a wave.
+/// </summary>
+/// <remarks>
+/// Contains information about the enemy prefab and its spawn probability,
+/// which determines the likelihood of each enemy type being spawned during a wave.
+/// </remarks>
 [Serializable]
 internal class EnemiesPerWaveSettings
 {
+    /// <summary>
+    /// Represents the prefab of an enemy used in spawning during a wave.
+    /// </summary>
     public Enemy enemyPrefab;
+
+    /// <summary>
+    /// Represents the probability of spawning a specific enemy in a wave configuration.
+    /// </summary>
     [SerializeField]
     [ProgressBar(1, 10, 0, 0.4f, 0.5f, Segmented = true)]
     [InfoBox("@\"Probability: \" + _spawnProbability * 10 + \"%\"")]
     private float _spawnProbability;
-    
+
+    /// <summary>
+    /// Retrieves the spawn probability for the associated enemy.
+    /// </summary>
+    /// <remarks>
+    /// Due to the <see cref="ProgressBarAttribute"/> behaviour, the <see cref="_spawnProbability"/> takes values from 1 to 10 for the probability.
+    /// </remarks>
+    /// <returns>
+    /// A float value representing the spawn probability of the enemy, scaled down to a range of 0.0 to 1.0.
+    /// </returns>
     public float GetSpawnProbability() => _spawnProbability / 10;
 }
 
+/// <summary>
+/// Represents the settings for a wave of enemy spawns in the game.
+/// </summary>
+/// <remarks>
+/// This class contains configuration for the number of enemies, their spawn interval, and specific details
+/// about the enemies to be spawned within the wave.
+/// Each wave can contain multiple types of enemies, each with a spawn probability.
+/// </remarks>
 [Serializable]
 internal class WaveSettings
 {
+    /// <summary>
+    /// Represents the total number of enemies to be spawned in a single wave.
+    /// This value determines how many enemy instances will be generated during
+    /// the course of the wave.
+    /// </summary>
     [SerializeField]
     public int numberOfEnemies;
+
+    /// <summary>
+    /// Specifies the interval, in seconds, between consecutive enemy spawns within a wave.
+    /// </summary>
     [SerializeField]
     public float spawnInterval;
+
+    /// <summary>
+    /// Represents the settings for enemies in a specific wave, containing various enemy configurations.
+    /// </summary>
+    /// <remarks>
+    /// This variable is part of the <see cref="WaveSettings"/> class and holds an array of <see cref="EnemiesPerWaveSettings"/> for that wave.
+    /// Each entry defines the type of enemy and its spawn probability within the specified wave.
+    /// </remarks>
     [SerializeField]
     public EnemiesPerWaveSettings[] enemies;
 }
 
+/// <summary>
+/// Manages and tracks the state of all enemies in the game.
+/// </summary>
+/// <remarks>
+/// This class utilizes the Singleton pattern.
+/// It provides functionality to manage the count of active enemies and update the state when an enemy is killed.
+/// </remarks>
 public class EnemiesController : MonoBehaviour
 {
+    /// <summary>
+    /// Singleton instance of the <see cref="EnemiesController"/> class.
+    /// </summary>
     public static EnemiesController Instance;
-    
+
+    /// <summary>
+    /// Represents the configuration for multiple waves of enemies in the game.
+    /// Each wave contains a specific number of enemies, spawn intervals,
+    /// and settings for enemy types and their spawn probabilities.
+    /// </summary>
+    /// <remarks>
+    /// Configured via the Inspector with support for list editing and
+    /// detailed settings for each wave.
+    /// </remarks>
     [SerializeField]
     [ListDrawerSettings(DefaultExpandedState = true, ShowFoldout = false, ShowIndexLabels = true)]
     private WaveSettings[] waves;
+
+    /// <summary>
+    /// Defines the radius within which enemies are spawned around the player.
+    /// This value is used to calculate a random spawn location near the player,
+    /// ensuring enemies are generated within a certain distance of the player's position.
+    /// </summary>
     [SerializeField]
     private float spawnRadius = 10f;
-    
+
+    /// <summary>
+    /// Specifies the percentage of enemies remaining at which a new wave of enemies is initiated.
+    /// </summary>
+    [SerializeField]
+    [Unit(Units.Percent)]
+    private int thresholdToStartNewWave = 10;
+
+    /// <summary>
+    /// Represents the index of the current wave in the enemy spawning system.
+    /// </summary>
     private int _currentWave;
-    private List<Enemy> _activeEnemies = new();
+
+    /// <summary>
+    /// Represents the count of currently active enemies in the game at any given time.
+    /// </summary>
     private int _activeEnemiesCount;
+
+    /// <summary>
+    /// Reference to the player's <see cref="Transform"/> component.
+    /// </summary>
     private Transform _player;
-    
+
+    /// <summary>
+    /// Initializes Singleton pattern by assigning Instance variable.
+    /// </summary>
     private void Awake()
     {
         if (!Instance || Instance != this)
@@ -52,7 +145,11 @@ public class EnemiesController : MonoBehaviour
             Instance = this;
         }
     }
-    
+
+    /// <summary>
+    /// Starts the initialization process for the enemies' controller.
+    /// This includes retrieving the player reference, initiating wave spawning through a coroutine, and storing the active enemies count for the current wave.
+    /// </summary>
     private void Start()
     {
         var playerObj = GameObject.FindGameObjectWithTag("Player");
@@ -68,11 +165,16 @@ public class EnemiesController : MonoBehaviour
         _activeEnemiesCount = waves[_currentWave].numberOfEnemies;
     }
 
+    /// <summary>
+    /// Updates the logic for progressing through waves of enemies in the game.
+    /// Checks the active enemy count against a threshold and advances to the next wave
+    /// if conditions are met. Begins spawning a new wave of enemies when progression occurs.
+    /// </summary>
     private void Update()
     {
         if (waves != null && waves.Length != 0)
         {
-            if (_activeEnemiesCount <= waves[_currentWave].numberOfEnemies * 0.1f && _currentWave <= waves.Length - 2)
+            if (_activeEnemiesCount <= waves[_currentWave].numberOfEnemies * thresholdToStartNewWave/100 && _currentWave <= waves.Length - 2)
             {
                 _currentWave++;
                 StartCoroutine(SpawnWave());
@@ -81,6 +183,11 @@ public class EnemiesController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Spawns a wave of enemies, based on the current wave settings.
+    /// Each enemy is instantiated at a random position within a specified spawn radius
+    /// around the player, and spawning is spaced out by the defined interval.
+    /// </summary>
     private IEnumerator SpawnWave()
     {
         if (waves == null || waves.Length == 0) yield break;
@@ -91,8 +198,7 @@ public class EnemiesController : MonoBehaviour
             var enemyPrefab = GetRandomPrefab(waves[_currentWave].enemies);
             if (randomSpawnPosition != Vector3.zero && enemyPrefab)
             {
-                var instantiatedEnemy = Instantiate(enemyPrefab, randomSpawnPosition, Quaternion.identity);
-                _activeEnemies.Add(instantiatedEnemy);
+                Instantiate(enemyPrefab, randomSpawnPosition, Quaternion.identity);
                 yield return new WaitForSeconds(waves[_currentWave].spawnInterval);
             }
             else
@@ -103,6 +209,14 @@ public class EnemiesController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Generates a random spawn position near the player within the defined spawn radius.
+    /// Ensures the position is valid using Unity's navigation mesh.
+    /// </summary>
+    /// <returns>
+    /// A <c>Vector3</c> representing a valid spawn position near the player.
+    /// If no valid position is found, returns <c>Vector3.zero</c>.
+    /// </returns>
     private Vector3 GetRandomSpawnPositionNearPlayer()
     {
         var randomDirection = _player.position + Random.insideUnitSphere * spawnRadius;
@@ -114,6 +228,13 @@ public class EnemiesController : MonoBehaviour
     }
 
 
+    /// <summary>
+    /// Selects a random enemy prefab from the provided array of enemy wave settings
+    /// based on their spawn probabilities.
+    /// </summary>
+    /// <param name="waveEnemiesSettings">An array of settings defining possible enemy prefabs
+    /// and their associated spawn probabilities for the current wave.</param>
+    /// <returns>The selected <see cref="Enemy"/> prefab if available, or null if no valid selection can be made.</returns>
     private Enemy GetRandomPrefab(EnemiesPerWaveSettings[] waveEnemiesSettings)
     {
         if (waveEnemiesSettings == null || waveEnemiesSettings.Length == 0)
@@ -140,9 +261,15 @@ public class EnemiesController : MonoBehaviour
         return null;
     }
 
-    public void EnemyKilled(Enemy enemy)
+    /// <summary>
+    /// Decreases the count of currently active enemies in the game.
+    /// </summary>
+    /// <remarks>
+    /// This method is called when an enemy is killed to update the internal count of active enemies.
+    /// It assumes that the enemy object is already destroyed or will be destroyed upon its invocation.
+    /// </remarks>
+    public void EnemyKilled()
     {
-        _activeEnemies.Remove(enemy);
         _activeEnemiesCount--;
     }
 }
--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,28 +1,80 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+/// <summary>
+/// Represents an enemy character within the game. Inherits from the <see cref="Health"/> class to include health-related behavior.
+/// </summary>
+/// <remarks>
+/// This class utilizes Unity's <see cref="NavMeshAgent"/> and <see cref="Animator"/> components for movement and animation.
+/// It is expected to be attached to enemy GameObjects.
+/// </remarks>
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(Animator))]
 public class Enemy : Health
 {
-    [SerializeField] private float damage;
-    [SerializeField] private float attackCooldown;
+    /// <summary>
+    /// The amount of damage that the enemy can inflict on a target during an attack.
+    /// </summary>
+    [Tooltip("The amount of damage that the enemy will inflict on a target during an attack.")]
+    [SerializeField] 
+    private float damage;
 
+    /// <summary>
+    /// The time interval, in seconds, that must pass before the enemy can attack again.
+    /// </summary>
+    [Tooltip("The time interval, in seconds, that must pass before the enemy can attack again.")]
+    [SerializeField]
+    private float attackCooldown;
+
+    /// <summary>
+    /// Represents the next time the enemy can perform an attack in seconds.
+    /// </summary>
     private float _nextAttackTime;
+
+    /// <summary>
+    /// Reference to the <see cref="NavMeshAgent"/> component attached to the enemy GameObject.
+    /// </summary>
     private NavMeshAgent _agent;
+
+    /// <summary>
+    /// Reference to the player's <see cref="Transform"/> component.
+    /// </summary>
     private Transform _player;
+
+    /// <summary>
+    /// Reference to the <see cref="Animator"/> component attached to the enemy GameObject.
+    /// </summary>
     private Animator _animator;
+
+    /// <summary>
+    /// Indicates whether the enemy is currently moving.
+    /// </summary>
     private bool _isMoving;
-    
+
+    /// <summary>
+    /// Represents the hash identifier for the "Moving" bool animation parameter.
+    /// </summary>
     private static readonly int MovingParam = Animator.StringToHash("Moving");
+
+    /// <summary>
+    /// Represents the hash identifier for the "Attack" animation trigger.
+    /// </summary>
     private static readonly int AttackParam = Animator.StringToHash("Attack");
 
+    /// <summary>
+    /// Initializes essential components for the Enemy class: <see cref="NavMeshAgent"/> and
+    /// <see cref="Animator"/>.
+    /// </summary>
     private void Awake()
     { 
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Initializes the enemy at the start of the game.
+    /// Attempts to locate the player GameObject using its tag "Player".
+    /// </summary>
     private void Start()
     {
         var playerObj = GameObject.FindGameObjectWithTag("Player");
@@ -36,23 +88,31 @@ public class Enemy : Health
         }
     }
 
+    /// <summary>
+    /// Handles movement towards the player, updates animations,
+    /// and set attack trigger when the enemy reaches the player and the attack cooldown has elapsed.
+    /// </summary>
     private void Update()
     {
         if (_player)
         {
             _agent.destination = _player.position;
-            _animator.SetBool(MovingParam, !IsAgentReachedDestination());
             if (IsAgentReachedDestination() && _nextAttackTime < Time.time)
             {
                 _animator.SetTrigger(AttackParam);
             }
+            _animator.SetBool(MovingParam, !IsAgentReachedDestination());
         }
         else
         {
             Debug.LogError("Player not found!");
         }
     }
-    
+
+    /// <summary>
+    /// Determines whether the NavMeshAgent has reached its destination.
+    /// </summary>
+    /// <returns>True if the agent has reached the destination or the path is complete; otherwise, false.</returns>
     private bool IsAgentReachedDestination()
     {
         return !_agent.pathPending &&
@@ -60,6 +120,16 @@ public class Enemy : Health
                (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f);
     }
 
+    /// <summary>
+    /// Performs an attack on the current player target if available.
+    /// Deals damage to the player and sets the cooldown for the next attack.
+    /// </summary>
+    /// <remarks>
+    /// The method uses the <see cref="Combat.ManageHit"/> function to apply damage to the player.
+    /// </remarks>
+    /// <example>
+    /// Use as the animation event to attack.
+    /// </example>
     public void Attack()
     {
         if (!_player)
@@ -71,10 +141,14 @@ public class Enemy : Health
         Combat.ManageHit(_player.gameObject, damage);
         _nextAttackTime = Time.time + attackCooldown;
     }
-    
+
+    /// <summary>
+    /// Decrements the count of active enemies through the <see cref="EnemiesController"/> and destroys
+    /// the enemy GameObject when health reaches 0.
+    /// </summary>
     public override void Kill()
     {
-        EnemiesController.Instance.EnemyKilled(this);
+        EnemiesController.Instance.EnemyKilled();
         Destroy(gameObject);
     }
 
